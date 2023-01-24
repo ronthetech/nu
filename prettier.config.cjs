13e648de9ea6fc34d3bd725682540ad35a14aa58
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-23 19:32:18 -0500

    edit: implement prettier tailwind plugin

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,8 @@
 /** @type {import("prettier").Config} */
 module.exports = {
+  plugins: [require.resolve("prettier-plugin-tailwindcss")],
   bracketSameLine: true,
   endOfLine: "lf",
   printWidth: 80,
   semi: false,
-};
+}
